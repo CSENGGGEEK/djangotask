962264a642f732ff280c9f4a017f32083ea5368d
Pseudo anonymous <anathon@anathon>
2023-11-27 08:51:47 +0530

    Login Funtionality Inprovised

--- Users/views.py
+++ Users/views.py
@@ -17,21 +17,31 @@ def signup(request):
 
     return render(request, 'signup.html', {'form': form})
 
-# View to login
 def login(request):
     if request.method == 'POST':
         form = CustomAuthenticationForm(request, request.POST)
         if form.is_valid():
-            username = form.cleaned_data['username']
+            username_or_email = form.cleaned_data['username_or_email']
             password = form.cleaned_data['password']
-            user = authenticate(request, username=username, password=password)
+            
+            # Check if the entered value is an email or username
+            if '@' in username_or_email:
+                # If it contains '@', consider it as an email
+                kwargs = {'email': username_or_email}
+            else:
+                # Otherwise, consider it as a username
+                kwargs = {'username': username_or_email}
+            
+            user = authenticate(request, **kwargs, password=password)
+            
             if user is not None:
                 auth_login(request, user)
-                return render(request=request,template_name="dashboard.html")
+                return render(request=request, template_name="dashboard.html")
     else:
         form = CustomAuthenticationForm()
 
     return render(request, 'login.html', {'form': form})
+
 
 def logout(request):
     return render(request, 'logout.html')
