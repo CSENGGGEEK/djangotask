b394d6479fc75affddcab61d4ec7c1915c225a7b
Pseudo anonymous <anathon@anathon>
2023-11-27 09:12:06 +0530

    Login Funtionality Inprovised

--- Users/forms.py
+++ Users/forms.py
@@ -10,28 +10,6 @@ class CustomUserCreationForm(UserCreationForm):
 
 class CustomAuthenticationForm(AuthenticationForm):
     username_or_email = forms.CharField(max_length=254, label='Username or Email')
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Remove the field not needed based on the context
-        if 'username' in self.fields:
-            del self.fields['username']
-        elif 'email' in self.fields:
-            del self.fields['email']
-
-    def clean(self):
-        cleaned_data = super().clean()
-        username_or_email = cleaned_data.get('username_or_email')
-        password = cleaned_data.get('password')
-
-        # Validate either username or email
-        if '@' in username_or_email:
-            self.cleaned_data['email'] = username_or_email
-        else:
-            self.cleaned_data['username'] = username_or_email
-        return cleaned_data
-
     class Meta:
         model = CustomUserModel
-        fields = ('username_or_email', 'password')
-
+        fields = ('password')
